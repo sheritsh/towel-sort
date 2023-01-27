832915dceb12e0a664dc64065f80bffada4ae888
Sherita Sherlene <sheritsh@un-h6.kzn.21-school.ru>
2023-01-28 02:59:58 +0300

    refactor: clang-style

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort(matrix) {
   let resMatrix = matrix;
   let resArr = [];
 
   for (i in resMatrix) {
-    if ( i % 2 != 0) {
-    resMatrix[i].reverse();
+    if (i % 2 != 0) {
+      resMatrix[i].reverse();
     }
   }
 
   for (j in resMatrix) {
     for (k in resMatrix[j]) {
-     resArr.push(resMatrix[j][k]);
+      resArr.push(resMatrix[j][k]);
     }
- 
-}
+  }
 
   return resArr;
 }
